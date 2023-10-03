86ff2f3280945e6bc55f807ab610cba15d099a30
Joachim Rørbøl <2394155+rorbol@users.noreply.github.com>
2023-10-03 15:14:32 +0200

    Removed organization teams due to new subscription model (#4)

--- main.tf
+++ main.tf
@@ -2,10 +2,3 @@ data "tfe_oauth_client" "github" {
   organization     = var.organization.organization
   service_provider = "github"
 }
-
-locals {
-  teams = try(transpose(merge([
-    for _, project in var.projects : {
-      for workspace_name, workspace in project.workspaces : workspace_name => workspace.tags
-  }]...)), [])
-}
--- organization.tf
+++ organization.tf
@@ -3,29 +3,3 @@ resource "tfe_organization" "this" {
   email                    = var.organization.email
   collaborator_auth_policy = "two_factor_mandatory"
 }
-
-resource "tfe_team" "this" {
-  for_each = local.teams
-
-  name         = each.key
-  organization = tfe_organization.this.name
-}
-
-resource "tfe_team_access" "this" {
-  for_each = { for index, value in flatten([for name, workspaces in local.teams : [
-    for _, w in workspaces : {
-      team      = name
-      workspace = w
-    }
-  ]]) : "${value.team}:${value.workspace}" => value }
-
-  access       = "admin"
-  team_id      = tfe_team.this[each.value.team].id
-  workspace_id = tfe_workspace.this[each.value.workspace].id
-}
-
-resource "tfe_team_token" "this" {
-  for_each = tfe_team.this
-
-  team_id = each.value.id
-}
